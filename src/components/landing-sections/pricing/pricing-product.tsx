3b617cd0f9911523d5e686de22463469709e111a
dvolkhonskiy <6569659+dvolkhonskiy@users.noreply.github.com>
2023-09-15 10:11:46 +0100

    fix li tag in pricing

--- src/components/landing-sections/pricing/pricing-product.tsx
+++ src/components/landing-sections/pricing/pricing-product.tsx
@@ -21,7 +21,7 @@ export default function PricingProduct({name, description, price, features}: {
                     {
                         features.map((feature, index) => {
                             return (
-                                <li className="flex items-center space-x-3">
+                                <li className="flex items-center space-x-3" key={feature + '-' + index}>
                                     <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
                                          fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path
